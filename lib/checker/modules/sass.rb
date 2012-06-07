6986382a2d62bdb3a87deb1fe640bba4079cfcc9
Jacek Jakubik <jacek.jakubik@netguru.pl>
2012-06-07 17:05:08 +0200

    sass module

---
+++ lib/checker/modules/sass.rb
@@ -0,0 +1,34 @@
+module Checker
+  module Modules
+    class Sass
+      def self.check
+        puts ">> Sass <<"
+
+        unless Sass.check_for_executable
+          puts "sass executable NOT FOUND, OMITTING..."
+          return true
+        end
+
+        files = Utils.files_modified
+        files.delete_if {|f| !f.ends_with?(".scss") or !f.ends_with?(".sass")}
+
+        files.map! do |f|
+          puts "Checking #{f}..."
+          Sass.check_one(f)
+        end
+
+        files.all_true?
+      end
+
+      def self.check_one(file)
+        system("sass #{file} > /dev/null")
+      end
+
+      def self.check_for_executable
+        cmd = "sass -v"
+        system(cmd)
+        $?.exitstatus == 0
+      end
+    end
+  end
+end
