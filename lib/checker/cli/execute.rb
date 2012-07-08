e7da3f2b728304c9b758d91df00d565b12fae138
Tomasz Pewiński <pewniak747@gmail.com>
2012-07-08 15:12:55 +0200

    moved methods to CLI class

--- lib/checker/cli/execute.rb
+++ lib/checker/cli/execute.rb
@@ -51,6 +51,25 @@ module Checker
           end
           exit (checked.all_true? ? 0 : 1)
         end
+
+        private
+        def available_modules
+          Checker::Modules.constants.map(&:to_s).map(&:downcase) - ['base']
+        end
+
+        def check_module_availability(modules)
+          constants = available_modules
+          result = modules - (constants & modules)
+          unless result.empty?
+            if block_given?
+              yield(result)
+            end
+          end
+        end
+
+        def get_modules_to_check
+          `git config checker.check`.chomp.split(",").map(&:strip)
+        end
       end
     end
   end
--- lib/checker/utils.rb
+++ lib/checker/utils.rb
@@ -4,23 +4,5 @@ module Utils
     @files_modified ||= `git status --porcelain | egrep "^(A |M |R ).*" | awk ' { if ($3 == "->") print $4; else print $2 } '`.split
     @files_modified.dup
   end
-
-  def available_modules
-    Checker::Modules.constants.map(&:to_s).map(&:downcase) - ['base']
-  end
-
-  def check_module_availability(modules)
-    constants = available_modules
-    result = modules - (constants & modules)
-    unless result.empty?
-      if block_given?
-        yield(result)
-      end
-    end
-  end
-
-  def get_modules_to_check
-    `git config checker.check`.chomp.split(",").map(&:strip)
-  end
 end
 end
