f4cdbca6806e3076b20336a81f06a1f2705ddc81
Tomasz Pewiński <pewniak747@gmail.com>
2012-07-10 14:32:37 +0200

    using less restrictive linter

--- lib/checker/modules/javascript.rb
+++ lib/checker/modules/javascript.rb
@@ -4,11 +4,26 @@ module Checker
       extensions 'js'
       private
       def check_one(file)
-        plain_command("jshint #{file}")
+        plain_command("jsl -process #{file}")
       end
 
       def check_for_executable
-        command('jshint -v', :show_output => false, :append => ">> /dev/null 2>&1")
+        command('jsl -help:conf', :show_output => false, :append => ">> /dev/null 2>&1")
+      end
+
+      def dependency_message
+        str = "Executable not found\n"
+        str << "Install jsl linter binary\n"
+        str << "More info: http://www.javascriptlint.com/download.htm\n"
+        str
+      end
+
+      # ignore exit status 1 - warnings
+      def plain_command cmd, options={}
+        system(cmd)
+        exitstatus = $?.exitstatus == 0 || $?.exitstatus == 1
+        show_output(exitstatus, options)
+        exitstatus
       end
     end
   end
