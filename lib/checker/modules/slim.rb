d742a4326ce039170bb9a9ac96f5e3d17a29221d
Filip Skokan <filip.s@mineus.net>
2012-08-09 06:57:05 +0200

    executable check

--- lib/checker/modules/slim.rb
+++ lib/checker/modules/slim.rb
@@ -7,6 +7,10 @@ module Checker
         plain_command("slimrb --compile #{file} >> /dev/null")
       end
 
+      def check_for_executable
+        command("slimrb -v", :use_bundler => true, :show_output => false, :append => ">> /dev/null 2>&1")
+      end
+
       def dependency_message
         str = "Executable not found\n"
         str << "Install slim from rubygems: 'gem install slim'\n"
