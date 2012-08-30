25f4e06cf6faa706a223c9467ab610cf613f9b99
Tomasz Pewiński <pewniak747@gmail.com>
2012-08-30 10:27:26 +0200

    fix specs

--- spec/checker/modules/base_spec.rb
+++ spec/checker/modules/base_spec.rb
@@ -4,23 +4,23 @@ describe Checker::Modules::Base do
   describe "rvm_command method" do
     context "properly fetches the ruby version from the environment variables" do
       before do
-        ENV.stub!(:fetch).with("rvm_path").and_return "/Users/test/.rvm"
+        ENV.stub!(:[]).with("rvm_path").and_return "/Users/test/.rvm"
       end
 
       it "uses no gemsets" do
-        ENV.stub!(:fetch).with("GEM_HOME").and_return "/Users/test/.rvm/gems/some_ruby_version"
+        ENV.stub!(:[]).with("GEM_HOME").and_return "/Users/test/.rvm/gems/some_ruby_version"
 
         Checker::Modules::Base.new.send(:rvm_command, "test").should == "/Users/test/.rvm/bin/rvm-shell 'some_ruby_version' -c 'test'"
       end
 
       it "uses global gemset" do
-        ENV.stub!(:fetch).with("GEM_HOME").and_return "/Users/test/.rvm/gems/some_ruby_version@global"
+        ENV.stub!(:[]).with("GEM_HOME").and_return "/Users/test/.rvm/gems/some_ruby_version@global"
 
         Checker::Modules::Base.new.send(:rvm_command, "test").should == "/Users/test/.rvm/bin/rvm-shell 'some_ruby_version@global' -c 'test'"
       end
 
       it "uses some other gemset" do
-        ENV.stub!(:fetch).with("GEM_HOME").and_return "/Users/test/.rvm/gems/some_ruby_version@v2"
+        ENV.stub!(:[]).with("GEM_HOME").and_return "/Users/test/.rvm/gems/some_ruby_version@v2"
 
         Checker::Modules::Base.new.send(:rvm_command, "test").should == "/Users/test/.rvm/bin/rvm-shell 'some_ruby_version@v2' -c 'test'"
       end
