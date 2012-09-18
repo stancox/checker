982dd363708ecff375f72bdbba6f9d00502b597b
Jacek Jakubik <jacek.jakubik@netguru.pl>
2012-09-18 11:20:57 +0200

    Specs for the #14

--- spec/checker/modules/base_spec.rb
+++ spec/checker/modules/base_spec.rb
@@ -25,5 +25,29 @@ describe Checker::Modules::Base do
         Checker::Modules::Base.new.send(:rvm_command, "test").should == "/Users/test/.rvm/bin/rvm-shell 'some_ruby_version@v2' -c 'test'"
       end
     end
+
+    context "using different rvm_path (system-wide rvm)" do
+      before do
+        ENV.stub!(:[]).with("rvm_path").and_return "/usr/lib/rvm"
+      end
+
+      it "uses no gemsets" do
+        ENV.stub!(:[]).with("GEM_HOME").and_return "/Users/test/.rvm/gems/some_ruby_version"
+
+        Checker::Modules::Base.new.send(:rvm_command, "test").should == "/usr/lib/rvm/bin/rvm-shell 'some_ruby_version' -c 'test'"
+      end
+
+      it "uses global gemset" do
+        ENV.stub!(:[]).with("GEM_HOME").and_return "/Users/test/.rvm/gems/some_ruby_version@global"
+
+        Checker::Modules::Base.new.send(:rvm_command, "test").should == "/usr/lib/rvm/bin/rvm-shell 'some_ruby_version@global' -c 'test'"
+      end
+
+      it "uses some other gemset" do
+        ENV.stub!(:[]).with("GEM_HOME").and_return "/Users/test/.rvm/gems/some_ruby_version@v2"
+
+        Checker::Modules::Base.new.send(:rvm_command, "test").should == "/usr/lib/rvm/bin/rvm-shell 'some_ruby_version@v2' -c 'test'"
+      end
+    end
   end
 end
